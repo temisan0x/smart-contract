950df81c36537c605081cec0b6b734d242e0c745
temycodes <temycodes@gmail.com>
2023-09-29 16:49:37 +0100

    build: day 2

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,30 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity 0.8.18; 
+pragma solidity 0.8.18; //stating out version
 
+contract SimpleStorage {
+    uint256 public myFavouriteNumber; //0
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPerson;//[]
+    mapping (string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favouriteNumber) public  {
+        myFavouriteNumber = _favouriteNumber;
+        retrieve();
+    }
+
+    //reads what the variable is 
+    //view, pure
+    function retrieve() public view returns (uint256){
+        return  myFavouriteNumber;
+    }
+
+     function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPerson.push( Person(_favoriteNumber, _name));
+    }
+
+}
