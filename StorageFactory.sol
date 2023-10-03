7d1c8beb5af11034eb25f57682a1f507743dc69d
temycodes <temycodes@gmail.com>
2023-10-03 13:23:33 +0100

    build: code cleannup

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
@@ -7,23 +8,25 @@ contract StorageFactory {
     //list of all contracts
     SimpleStorage[] public listOfSimpleStorageContracts;
 
-    function createSimpleStorageContract() public  {
+    function createSimpleStorageContract() public {
         //when we call a simpleStorage -> we deploy a newSimpleStorageContract
         SimpleStorage simpleStorageContractVariable = new SimpleStorage();
         listOfSimpleStorageContracts.push(simpleStorageContractVariable);
     }
 
     function sfStore(
-        uint256 _simpleStorageIndex, 
+        uint256 _simpleStorageIndex,
         uint256 _simpleStorageNumber
-        ) public { // Address
+    ) public {
+        // Address
         // ABI
-        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[
+            _simpleStorageIndex
+        ];
         mySimpleStorage.store(_simpleStorageNumber);
     }
 
-    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
-        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
-        return mySimpleStorage.retrieve();
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
