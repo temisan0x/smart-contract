a88c69e9c2ffdb6c8de78af7066f2f0643cebfe1
temycodes <temycodes@gmail.com>
2023-10-30 10:29:11 +0100

    check ~ chainlink oracle contract

--- lesson1/AddFiveStorage.sol
+++ lesson1/AddFiveStorage.sol
@@ -1,10 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.18;
+pragma solidity ^0.8.18; 
 
-import {SimpleStorage} from "./SimpleStorage.sol";
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
 
-contract AddFiveStorage is SimpleStorage {
-    function store(uint256 _newNumber) public  virtual override {
-        myFavoriteNumber = _newNumber + 5;
-    } 
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+contract FundMe {
+    function fund() public payable  {
+        require(msg.value > 1e18, "didn't send the transaction");
+    }
+
+    function withdraw () public  {
+        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
+
+    function getVersion() public view returns(uint256){
+      return  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
