d7d5dbff69125c37bb7a67544cdaa86cf44e2dec
temycodes <temycodes@gmail.com>
2023-10-31 09:01:30 +0100

    build: ensuring contribution meets the standard min value USD

--- lesson1/AddFiveStorage.sol
+++ lesson1/AddFiveStorage.sol
@@ -18,16 +18,53 @@ interface AggregatorV3Interface {
     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
 
+
 contract FundMe {
+
+    event PriceReceived(int256 price); 
+    uint256 public mininumUsd = 5e18; //5 eth(in wei)
+    //50000000000000000000
+
     function fund() public payable  {
-        require(msg.value > 1e18, "didn't send the transaction");
+      //Allow users to send $
+      //Have a minumum $ snet $5
+        require(getConversionRate(msg.value) >= mininumUsd, "didn't send the transaction");
     }
 
-    function withdraw () public  {
-        //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    function withdraw() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
     }
 
-    function getVersion() public view returns(uint256){
-      return  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    function getVersion() public view returns(uint256) {
+      return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+    function logPrice() public {
+       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+       (, int256 price, , ,) = priceFeed.latestRoundData();
+       
+       // Answer = price of eth in USD
+       // Log the received price for debugging
+      emit PriceReceived(price);
+       
+      require(price >= 0, "Negative price not supported");
+    }
+
+      function getPrice() public view returns(uint256) {
+       AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+       (, int256 price, , ,) = priceFeed.latestRoundData();
+      
+       return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+      uint256 ethPrice = getPrice();
+      uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
+      return ethAmountInUsd;
     }
 }
+ 
+ 
+//returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+//uint256: 1829445376950000000000
+//uint256: 182944537695
