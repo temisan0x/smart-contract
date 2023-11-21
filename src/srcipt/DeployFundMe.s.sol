405fcdafb4440a32ecf3e005d331596f91403fea
temycodes <temycodes@gmail.com>
2023-11-21 11:14:46 +0100

    build: code cleanup

---
+++ src/srcipt/DeployFundMe.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+// Author: Temisan Momodu
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../lesson2/FundMe.sol";
+
+contract DeployFundMe is Script {
+    // Deploy a new instance of the FundMe contract
+    function run() external returns (FundMe) {
+        // Start broadcasting transactions
+        vm.startBroadcast();
+
+        // Deploy FundMe with a specific Chainlink Price Feed address
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+
+        // Return the deployed FundMe contract
+        return fundMe;
+    }
+}
